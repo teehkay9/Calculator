a2d68591fb72833db20762401c3ed7c9fb78159e
Thomas <thomashoelsken@yahoo.de>
2024-07-25 05:47:04 +0200

    Add function and event listener
    - added the function calculate which mimics the calculation
    - added eventlistener for operand buttons and clear button

--- script.js
+++ script.js
@@ -13,6 +13,7 @@ const equalsButton = document.querySelector(".equals");
 const clearButton = document.querySelector(".clear");
 const decimalButton = document.querySelector(".decimal");
 
+// functions
 function updateDisplay(value) {
   display.textContent = value;
 }
@@ -20,3 +21,56 @@ function updateDisplay(value) {
 function getDisplayValue() {
   return display.textContent;
 }
+
+function calculate() {
+  let result;
+  const prev = previousOperand;
+  const curr = currentOperand;
+  if (isNaN(prev) || isNaN(curr)) return;
+
+  switch (operator) {
+    case "+":
+      result = prev + curr;
+      break;
+    case "-":
+      result = prev + curr;
+      break;
+    case "/":
+      result = prev / curr;
+      break;
+    case "*":
+      result = prev * curr;
+      break;
+    default:
+      return;
+  }
+  currentOperand = result.toString(); // store result in currentOperand
+  previousOperand = ""; // clear previousOperand
+  updateDisplay(currentOperand);
+  shouldResetDisplay = true; // Set the flag to reset display for new entry
+}
+
+// event listener
+numberButtons.forEach((button) => {
+  button.addEventListener("click", () => {
+    if (shouldResetDisplay) {
+      currentOperand = button.value;
+      shouldResetDisplay = false;
+    } else {
+      if (getDisplayValue() === "0" && button.value !== ".") {
+        currentOperand = button.value;
+      } else {
+        currentOperand += button.value;
+      }
+    }
+    updateDisplay(currentOperand);
+  });
+});
+
+clearButton.addEventListener("click", () => {
+  currentOperand = "";
+  previousOperand = "";
+  operator = null;
+  shouldResetDisplay = false;
+  updateDisplay("0");
+});
